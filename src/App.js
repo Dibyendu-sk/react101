9e9fffbfb40ac31cee394b4fe4f75ce0f1bcccc4
Dibyendu <dibyendu01kar@gmail.com>
2024-04-14 11:05:02 +0530

    Using event handler

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import ProfileCard from "./ProfileCard";
-import AlexaLogo from "./images/alexa.png";
-import CortanaLogo from "./images/cortana.png";
-import SiriLogo from "./images/siri.png";
-import "bulma/css/bulma.css"
-function App(){
-    return(
-        <div>
-            <section className="hero is-primary is-small">
-                <div className="hero-body">
-                    <p className="title">Personal AI Assistance</p>
-                </div>
-            </section>
-            <div className="container">
-                <section>
-                    <div className="columns">
-                        <div className="column is-3"><ProfileCard
-                            title="title1"
-                            handle="@handle1"
-                            image={AlexaLogo}
-                            description={"Made By Amazon"}
-                        />
-                        </div>
-                        <div className="column is-3"><ProfileCard
-                            title="title2"
-                            handle="@handle2"
-                            image={SiriLogo}
-                            description={"Made By Apple"}
-                        />
-                        </div>
-                        <div className="column is-3"><ProfileCard
-                            title="title3"
-                            handle="@handle3"
-                            image={CortanaLogo}
-                            description={"Made By Microsoft"}
-                        />
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
-    )
-}
 
+function App(){
+    const handleClick=()=>{
+        alert("Button Was Clicked");
+    }
+    return <div>
+        {/*<button onClick={handleClick}>Add Animal</button>*/}
+        {/*we can also write ir like this*/}
+        <button onClick={()=>alert("Button Was Clicked")}>Add Animal</button>
+    </div>
+}
 export default App;
